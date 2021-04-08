b25a7867dd556dcedb79246f4352d2a0d42a8eb7
jotawarsd <jotawarsd@gmail.com>
2021-04-09 01:02:58 +0530

    Update reverse.py

--- assignments_sem1/reverse.py
+++ assignments_sem1/reverse.py
@@ -2,11 +2,11 @@
 
 '''
 
-str1 = input("number : ")   #get input from user
+num1 = input("number : ")   #get input from user
 
 def reverse(s1):
     n = len(s1) - 1   #establish index of last digit
     for i in range(n,-1,-1):    #printing the number in reverse
         print(s1[i], end='')
 
-reverse(str1)
+reverse(num1)
