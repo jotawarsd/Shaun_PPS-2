6721d2c6497b2a422dcb6714f998ba89a0fa4d7a
jotawarsd <jotawarsd@gmail.com>
2021-05-03 11:06:21 +0530

    updates and debugs

--- cash.py
+++ cash.py
@@ -1,9 +1,14 @@
 n = float(input("amount of cash: "))
-deno = [1, 5, 10, 20, 50, 100, 200, 500, 2000]
+deno = [0, 1, 5, 10, 20, 50, 100, 200, 500, 2000]
 x = 0
-for i in range(8,0,-1):
-    while (n < deno[i] and n >= deno[i-1]):
-        n = n - deno[i]
-        x = x + 1
-print(x)
 
+for i in range(9,0,-1):
+    if (n >= deno[i]):
+        while (n >= deno[i]):
+            n = n - deno[i]
+            x += 1
+    else:
+        continue
+
+print(x)
+print(n)
